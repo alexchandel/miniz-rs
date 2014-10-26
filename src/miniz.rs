bb9e57d70029503ef920ad7f6e7d568d2cd328e0
Alex Chandel <alexchandel@gmail.com>
2014-10-25 22:24:18 -0400

    Clear object with iterator

--- src/miniz.rs
+++ src/miniz.rs
@@ -1968,7 +1968,7 @@ fn tdefl_compress(d: &mut tdefl_compressor, pIn_buf: *const u8, pIn_buf_size: *c
       return d.m_prev_return_status;
     }
     d.m_finished = (flush == TDEFL_FINISH);
-    if (flush == TDEFL_FULL_FLUSH) { MZ_CLEAR_OBJ(d.m_hash); MZ_CLEAR_OBJ(d.m_next); d.m_dict_size = 0; }
+    if flush == TDEFL_FULL_FLUSH { for i in d.m_hash.iter_mut() {i = 0u16;}; for i in d.m_next.iter_mut() {i = 0u16;}; d.m_dict_size = 0; }
   }
 
   return (d.m_prev_return_status = tdefl_flush_output_buffer(d));
@@ -1991,7 +1991,7 @@ fn tdefl_init(d: &mut tdefl_compressor, pPut_buf_func: tdefl_put_buf_func_ptr, p
   d.m_pPut_buf_func = pPut_buf_func; d.m_pPut_buf_user = pPut_buf_user;
   d.m_flags = (mz_uint)(flags); d.m_max_probes[0] = 1 + ((flags & 0xFFF) + 2) / 3; d.m_greedy_parsing = (flags & TDEFL_GREEDY_PARSING_FLAG) != 0;
   d.m_max_probes[1] = 1 + (((flags & 0xFFF) >> 2) + 2) / 3;
-  if (!(flags & TDEFL_NONDETERMINISTIC_PARSING_FLAG)) {MZ_CLEAR_OBJ(d.m_hash);}
+  if (!(flags & TDEFL_NONDETERMINISTIC_PARSING_FLAG)) {for i in d.m_hash.iter_mut() {i = 0};}
   d.m_lookahead_pos = d.m_lookahead_size = d.m_dict_size = d.m_total_lz_bytes = d.m_lz_code_buf_dict_pos = d.m_bits_in = 0;
   d.m_output_flush_ofs = d.m_output_flush_remaining = d.m_finished = d.m_block_index = d.m_bit_buffer = d.m_wants_to_finish = 0;
   d.m_pLZ_code_buf = d.m_lz_code_buf + 1; d.m_pLZ_flags = d.m_lz_code_buf; d.m_num_flags_left = 8;
