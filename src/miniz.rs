30269127dd38dbf3f90e23cc553c587a2ffe5c98
Alex Chandel <alexchandel@gmail.com>
2014-11-02 04:44:56 -0500

    Mutability fix in adler32

--- src/miniz.rs
+++ src/miniz.rs
@@ -322,7 +322,7 @@ fn mz_crc32(crc: mz_ulong, buf: Option<&[u8]>) -> mz_ulong
   let mut buf: &[u8] = match buf { Some(x) => x, None => return MZ_CRC32_INIT };
   let s_crc32: [u32, ..16] = [ 0, 0x1db71064, 0x3b6e20c8, 0x26d930ac, 0x76dc4190, 0x6b6b51f4, 0x4db26158, 0x5005713c,
     0xedb88320, 0xf00f9344, 0xd6d6a3e8, 0xcb61b38c, 0x9b64c2b0, 0x86d3d2d4, 0xa00ae278, 0xbdbdf21c ];
-  let crcu32: u32 = crc as u32;
+  let mut crcu32: u32 = crc as u32;
   crcu32 = !crcu32;
   for b in buf.iter() {
     crcu32 = (crcu32 >> 4) ^ s_crc32[((crcu32 as u8 & 0xF) ^ (*b & 0xF)) as uint];
